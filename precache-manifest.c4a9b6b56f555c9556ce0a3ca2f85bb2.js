a579b1401bf703c129c241d89def92855c784c50
Chommy92 <chommyukatu92@gmail.com>
2020-06-28 03:41:41 +0300

    Updates

---
+++ precache-manifest.c4a9b6b56f555c9556ce0a3ca2f85bb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0b4f4e2ff12cbdf7457e669f3f8017",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "699ee71815c844385074",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "755bc1ef84aca4914671",
+    "url": "/robofriends/static/css/main.1fd9534e.chunk.css"
+  },
+  {
+    "revision": "699ee71815c844385074",
+    "url": "/robofriends/static/js/2.ba5e6a5c.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/robofriends/static/js/2.ba5e6a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755bc1ef84aca4914671",
+    "url": "/robofriends/static/js/main.70d750f4.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
